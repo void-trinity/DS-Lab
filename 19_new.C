16f3eecf5d20982ddb82c5e9e261e42389787597
Siddharth Chandra <void-trinity@users.noreply.github.com>
2018-04-01 20:13:44 +0530

    Corrected 19, queue

---
+++ 19_new.C
@@ -0,0 +1,84 @@
+// Linked List implementation of Queue
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*front = NULL,*rear = NULL;
+
+void enQueue(int value)
+{
+    struct Node *newNode;
+    newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    if(rear == NULL){
+        front = rear = newNode;
+    }
+    else {
+        rear = rear->next = newNode;
+    }
+    printf("Insertion Successful !!\n");
+}
+
+void deQueue()
+{
+    if(front==NULL)
+        printf("Queue is Empty !!\n");
+    else
+    {
+        struct Node *temp=front;
+        printf("Deleted element: %d\n",temp->data);
+        if(front == rear)
+            front = rear = NULL;
+        else {
+            front=front->next;
+        }
+        free(temp);
+    }
+}
+void display()
+{
+    if(front==NULL && rear==NULL)
+        printf("Queue is Empty !!\n");
+    else
+    {
+        struct Node *temp;
+        temp = front;
+        printf("Queue : ");
+        while(temp!=NULL)
+        {
+            printf("%d --> ",temp->data);
+            temp = temp -> next;
+        }
+    }
+}
+
+int main()
+{
+    int ch,val;
+    printf("Siddharth Chandra, 16115078\n");
+    while(1)
+    {
+        printf("\nAvailable Choices \n");
+        printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1 : printf("Enter Value for Insertion : ");
+                scanf("%d", &val);
+                enQueue(val);
+                break;
+            case 2 : deQueue();
+                break;
+            case 3 : display();
+                break;
+            case 4 : exit(0);
+            default: printf("\nInvalid Choice !! Please try again...\n");
+        }
+    }
+    return 0;
+}
